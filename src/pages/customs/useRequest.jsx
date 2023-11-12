75511cd171eae8d2a9cd61cc783525971bfe6138
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-11 17:30:13 -0800

    Created a blueprint

--- src/pages/customs/useRequest.jsx
+++ src/pages/customs/useRequest.jsx
@@ -2,92 +2,52 @@ import { useCookies } from "react-cookie";
 import axios from "axios";
 
 const useRequest = () => {
-  const [, setCookie, removeCookie] = useCookies(["session"]);
+  const [cookie, setCookie, removeCookie] = useCookies(["session"]);
 
   const getConfig = (data) => {
-    if (!data) {
-      return {
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-    }
-    return {
+    const config = {
       mode: "cors",
       headers: {
         "Content-Type": "application/json",
       },
       data: data,
     };
+    return config;
   };
 
-  const getRequest = async (url, data = null) => {
+  const getRequest = async (url, data = undefined) => {
     try {
       const response = await axios.get(url, getConfig(data));
-      if (response.status === 200) {
-        return response.data;
-      }
-      if (response.status === 401) {
-        removeCookie("session");
-        alert("Invalid session");
-        window.location.href = "/404";
-        return;
-      }
-      if (response.status === 403 || response.status === 404) {
-        window.location.href = "/404";
-      }
     } catch (e) {
-      if (e.response.status === 401) {
-        removeCookie("session");
-        window.location.href = "/404";
-      }
-      if (e.response.status === 500) {
-        alert("Internal server error. Please try again!");
-        window.location.reload();
-      }
-      if (e.response.status === 403 || e.response.status === 404) {
-        window.location.href = "/404";
-      }
+      console.log(e);
     }
   };
 
-  const postRequest = async (url, data = null) => {
+  const postRequest = async (url, data = undefined) => {
     try {
       const response = await axios.post(url, getConfig(data));
-      if (response.status === 200) {
-        return response.data;
-      }
-      if (response.status === 403 || response.status === 404) {
-        window.location.href = "/404";
-      }
     } catch (e) {
-      if (e.response.status === 401) {
-        removeCookie("session");
-        window.location.href = "/404";
-      }
-      if (e.response.status === 403 || e.response.status === 404) {
-        window.location.href = "/404";
-      }
-      if (e.response.status === 500) {
-        alert("Internal server error. Please try again!");
-        window.location.reload();
-      }
+      console.log(e);
     }
+  };
+
+  const setUpCookie = () => {
+    const expireTime = 60 * 60 * 1000;
+    setCookie("session", sessionID, {
+      path: "/",
+      maxAge: expireTime,
+      sameSite: "strict",
+    });
   };
 
   const logInRequest = async (credentials) => {
     const url = import.meta.env.VITE_API + "user/login";
     const sessionID = await postRequest(url, credentials);
     if (sessionID) {
-      const expireTime = 60 * 60 * 1000;
-      setCookie("session", sessionID, {
-        path: "/",
-        maxAge: expireTime,
-        sameSite: "strict",
-      });
+      setUpCookie();
+      return true;
     }
-    return sessionID;
+    return false;
   };
 
   const logOutRequest = async () => {
