fd558a4e7d28a4d398e7a7cbeb7b01bcc567fac7
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-11 18:22:38 -0800

    Created a simple routes - Home, signup and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">App</h1>;
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="/login" exact element={<LogIn />} />
+        <Route path="/signup" exact element={<SignUp />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={routes} />;
 };
 
 export default App;
