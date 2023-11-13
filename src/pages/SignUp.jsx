e3393e88177e6061b8ec79e64398bc978e2f8bc6
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-13 14:04:27 -0800

    Created a simply sign up page without any designs

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import useRequest from "./customs/useRequest";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { postRequest } = useRequest();
+
+  const signUp = () => {
+    console.log("signUp");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button onClick={signUp}> </button>
+    </div>
+  );
 };
 
 export default SignUp;
