6700b6f96d9048a814d7814d0167ef68c60ae976
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-13 21:59:43 -0800

    Implemented the sign up logic

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
 import useRequest from "./customs/useRequest";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
   const { postRequest } = useRequest();
 
-  const signUp = () => {
-    console.log("signUp");
+  const signUp = async () => {
+    const body = { username, password, email };
+    try {
+      const success = await postRequest("auth/register", body);
+      if (success) {
+        console.log("success");
+        navigate("/login");
+      } else {
+        console.log("fail");
+      }
+    } catch (error) {
+      if (error.response.status === 409) {
+        console.log("Username or email already in exists");
+      }
+    }
   };
 
   return (
-    <div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center gap-5">
       <input
+        className="ring-2 ring-gray-200 rounded-md px-2 py-1 w-1/4"
+        placeholder="Enter your username"
         type="text"
         value={username}
         onChange={(e) => {
@@ -20,13 +38,30 @@ const SignUp = () => {
         }}
       />
       <input
+        className="ring-2 ring-gray-200 rounded-md px-2 py-1 w-1/4"
+        placeholder="Enter your email"
+        type="email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        className="ring-2 ring-gray-200 rounded-md px-2 py-1 w-1/4"
+        placeholder="Enter your password"
         type="password"
         value={password}
         onChange={(e) => {
           setPassword(e.target.value);
         }}
       />
-      <button onClick={signUp}> </button>
+      <button
+        className="bg-blue-500 text-white px-2 py-1 rounded-md"
+        onClick={signUp}
+      >
+        {" "}
+        Sign Up{" "}
+      </button>
     </div>
   );
 };
