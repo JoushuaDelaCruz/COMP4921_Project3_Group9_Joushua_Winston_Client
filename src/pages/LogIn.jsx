5bb33ddb975eb7d6591c550913d99e48dc94a6f0
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-13 23:10:33 -0800

    Implemented the login system

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useRequest from "./customs/useRequest";
 
-const LogIn = ({ setUser }) => {
-  return <div>LogIn</div>;
+const LogIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { logInRequest } = useRequest();
+  const navigation = useNavigate();
+
+  const login = async () => {
+    const credentials = { email, password };
+    const success = await logInRequest(credentials);
+    if (success) {
+      navigation("/");
+    } else {
+      console.log("fail");
+    }
+  };
+
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center gap-5">
+      <input
+        className="ring-2 ring-gray-200 rounded-md px-2 py-1 w-1/4"
+        placeholder="Enter your email"
+        type="email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        className="ring-2 ring-gray-200 rounded-md px-2 py-1 w-1/4"
+        placeholder="Enter your password"
+        type="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button
+        className="bg-blue-500 text-white px-2 py-1 rounded-md"
+        onClick={login}
+      >
+        {" "}
+        Log In{" "}
+      </button>
+    </div>
+  );
 };
 
 export default LogIn;
