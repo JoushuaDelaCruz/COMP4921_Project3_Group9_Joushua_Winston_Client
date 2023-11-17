19b2a10a3424a85a04053a9e7b62e77281ae0e86
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-16 17:04:21 -0800

    Added custom fontFamily,, animations and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto Slab"],
+      },
+      colors: {
+        feldgrau: "#3c493f",
+        "battleship-grey": "#7E8D85",
+        "ash-grey": "#B3BFB8",
+        "mint-cream": "#F0F7F4",
+        celadon: "#A2E3C4",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+        "bounce-once": "bounce 1s ease-in-out 1",
+        "bounce-twice": "bounce 1s ease-in-out 2",
+      },
+    },
   },
   plugins: [],
 };
