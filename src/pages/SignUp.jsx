848ac7035a490f84caf7dbdb4b809936455da82a
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-17 20:26:47 -0800

    Added front-end security and feedback to user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,19 +6,77 @@ const SignUp = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [isUsername, setIsUsername] = useState(false);
-  const [isPassword, setIsPassword] = useState(false);
-  const [isEmail, setIsEmail] = useState(false);
+  const [usernameErr, setUsernameErr] = useState(null);
+  const [passwordErr, setPasswordErr] = useState(null);
+  const [emailErr, setEmailErr] = useState(null);
 
   const navigate = useNavigate();
-  const { postRequest } = useRequest();
+  const { getRequest, postRequest } = useRequest();
 
-  const signUp = async () => {
+  const validateUsername = async () => {
+    if (username.length < 1) {
+      setUsernameErr("Please enter a username");
+      return false;
+    }
+    const usernameExists = await getRequest(
+      `auth/checkUsernameExists/${username}`
+    );
+    if (usernameExists) {
+      setUsernameErr("Username already exists!");
+      return false;
+    }
+    setUsernameErr(null);
+    return true;
+  };
+
+  const validatePassword = async () => {
+    if (password.length < 1) {
+      setPasswordErr("Please enter a password");
+      return false;
+    }
+    if (password.length <= 10) {
+      setPasswordErr("Password must be at least 10 characters long");
+      return false;
+    }
+    if (!password.match(/[a-z]/)) {
+      setPasswordErr("Password must contain a lowercase letter");
+      return false;
+    }
+    if (!password.match(/[A-Z]/)) {
+      setPasswordErr("Password must contain an uppercase letter");
+      return false;
+    }
+    if (!password.match(/[0-9]/)) {
+      setPasswordErr("Password must contain a number");
+      return false;
+    }
+    if (!password.match(/[^a-zA-Z\d]/)) {
+      setPasswordErr("Password must contain a special character");
+      return false;
+    }
+    setPasswordErr(null);
+    return true;
+  };
+
+  const validateEmail = async () => {
+    if (email.length < 1 || !email.includes("@")) {
+      setEmailErr("Please enter an email");
+      return false;
+    }
+    const emailExists = await getRequest(`auth/checkEmailExists/${email}`);
+    if (emailExists) {
+      setEmailErr("Email already exists!");
+      return false;
+    }
+    setEmailErr(null);
+    return true;
+  };
+
+  const signUpUserReq = async () => {
     const body = { username, password, email };
     try {
       const success = await postRequest("auth/register", body);
       if (success) {
-        console.log("success");
         navigate("/login");
       } else {
         console.log("fail");
@@ -30,14 +88,26 @@ const SignUp = () => {
     }
   };
 
+  const signUp = async () => {
+    const isUsernameValid = await validateUsername();
+    const isEmailValid = await validateEmail();
+    const isPasswordValid = await validatePassword();
+
+    if (!isUsernameValid || !isEmailValid || !isPasswordValid) {
+      return;
+    }
+
+    await signUpUserReq();
+  };
+
   const goToHome = () => {
     navigate("/");
   };
 
   return (
-    <div className="h-screen w-screen flex flex-col items-center gap-5 bg-mint-cream sm:justify-center">
-      <div className="w-full flex flex-col gap-5 font-roboto bg-feldgrau rounded-sm sm:rounded-md text-white max-w-2xl py-5 shadow-lg shadow-slate-800">
-        <header className="text-3xl border-b text-center border-gray-400 pb-5 font-roboto font-semibold w-full">
+    <div className="h-screen w-screen flex flex-col items-center gap-5 bg-mint-cream sm:justify-center font-roboto">
+      <div className="w-full flex flex-col gap-5 bg-feldgrau rounded-sm sm:rounded-md text-white max-w-2xl py-5 shadow-lg shadow-slate-800">
+        <header className="text-3xl border-b text-center border-gray-400 pb-5 font-semibold w-full">
           <h1
             className=" hover:transform hover:transition-all hover:duration-100 hover:animate-bounce cursor-pointer"
             onClick={goToHome}
@@ -45,23 +115,38 @@ const SignUp = () => {
             EvenTogether
           </h1>
         </header>
-        <div className="flex flex-col gap-6 items-center justify-center px-5 font-medium text-sm text-gray-400">
-          <div className="flex flex-col gap-1 w-full max-w-sm">
-            <label htmlFor="email">Username</label>
+        <div className="flex flex-col gap-3 items-center justify-center px-5 font-medium text-sm">
+          <div
+            className={`flex flex-col gap-1 w-full max-w-sm ${
+              !usernameErr ? "text-gray-400" : "text-red-500"
+            } relative`}
+          >
+            <label htmlFor="username">Username</label>
             <input
-              className="peer ring-2 ring-gray-200 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-600 transform transition-all duration-500 hover:scale-x-105"
-              placeholder="Enter your email"
+              className={`peer ring-2 ${
+                !usernameErr ? "ring-gray-200" : "ring-red-500"
+              } rounded px-3 py-2 focus:outline-none focus:ring-2 text-gray-700 focus:ring-green-600 transform transition-color duration-200`}
+              placeholder="Enter your username"
               type="text"
               value={username}
               onChange={(e) => {
                 setUsername(e.target.value);
               }}
             />
+            <h3 className={!usernameErr ? `text-transparent` : `text-current`}>
+              {usernameErr || "Placeholder"}
+            </h3>
           </div>
-          <div className="flex flex-col gap-1 w-full max-w-sm">
+          <div
+            className={`flex flex-col gap-1 w-full max-w-sm ${
+              !emailErr ? "text-gray-400" : "text-red-500"
+            }`}
+          >
             <label htmlFor="email">Email</label>
             <input
-              className="peer ring-2 ring-gray-200 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-600 transform transition-all duration-500 hover:scale-x-105"
+              className={`peer ring-2 ${
+                !emailErr ? "ring-gray-200" : "ring-red-500"
+              } rounded px-3 py-2 focus:outline-none focus:ring-2 text-gray-700 focus:ring-green-600 transform transition-color duration-200`}
               placeholder="Enter your email"
               type="email"
               value={email}
@@ -69,11 +154,20 @@ const SignUp = () => {
                 setEmail(e.target.value);
               }}
             />
+            <h3 className={!emailErr ? `text-transparent` : `text-current`}>
+              {emailErr || "Placeholder"}
+            </h3>
           </div>
-          <div className="flex flex-col gap-1 w-full max-w-sm">
+          <div
+            className={`flex flex-col gap-1 w-full max-w-sm ${
+              !passwordErr ? "text-gray-400" : "text-red-500"
+            }`}
+          >
             <label htmlFor="password">Password</label>
             <input
-              className="ring-2 ring-gray-200 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-600 transform transition-all duration-500 hover:scale-x-105"
+              className={`peer ring-2 ${
+                !passwordErr ? "ring-gray-200" : "ring-red-500"
+              } rounded px-3 py-2 focus:outline-none focus:ring-2 text-gray-700 focus:ring-green-600 transform transition-color duration-200`}
               placeholder="Enter your password"
               type="password"
               value={password}
@@ -81,9 +175,12 @@ const SignUp = () => {
                 setPassword(e.target.value);
               }}
             />
+            <h3 className={!passwordErr ? `text-transparent` : `text-current`}>
+              {passwordErr || "Placeholder"}
+            </h3>
           </div>
         </div>
-        <footer className="w-full flex justify-center mt-5">
+        <footer className="w-full flex justify-center mt-2">
           <button
             className=" bg-green-500 text-white rounded-full px-10 py-2 font-medium hover:bg-green-700 focus:bg-green-700 active:bg-green-900 transform transition-colors duration-200"
             onClick={signUp}
