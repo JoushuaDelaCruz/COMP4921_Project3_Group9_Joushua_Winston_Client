ccbb6640d887fe41c0b39549307bf94290c6644e
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-17 23:24:05 -0800

    Created a checksession to check if session is still valid

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Home from "./pages/Home";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
+import { useEffect, useState } from "react";
+import useRequest from "./pages/customs/useRequest";
 import {
   Route,
   createBrowserRouter,
@@ -9,12 +11,36 @@ import {
 } from "react-router-dom";
 
 const App = () => {
+  const [userAuth, setUserAuth] = useState(false);
+  const { getRequest, logOutRequest, logInRequest } = useRequest();
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const sessionValidity = await getRequest("auth/verify");
+      if (!sessionValidity) {
+        setUserAuth(false);
+        await logOutRequest();
+      } else {
+        setUserAuth(true);
+      }
+    };
+
+    checkSession();
+  }, [logOutRequest, logInRequest]);
+
+  const isUserAuth = async () => {
+    if (userAuth) {
+      window.location.href = "/";
+    }
+    return null;
+  };
+
   const routes = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/">
-        <Route index element={<Home />} />
-        <Route path="/login" exact element={<LogIn />} />
-        <Route path="/signup" exact element={<SignUp />} />
+        <Route index element={<Home userAuth={userAuth} />} />
+        <Route path="/login" loader={isUserAuth} exact element={<LogIn />} />
+        <Route path="/signup" loader={isUserAuth} exact element={<SignUp />} />
       </Route>
     )
   );
