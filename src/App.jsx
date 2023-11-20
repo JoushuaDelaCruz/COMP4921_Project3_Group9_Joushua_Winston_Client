89cd086b22de1cef05699dc42b034600877bc81a
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-19 19:48:41 -0800

    Created the profile router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
 import { useEffect, useState } from "react";
 import useRequest from "./pages/customs/useRequest";
+import Profile from "./pages/profile";
 import {
   Route,
   createBrowserRouter,
@@ -35,12 +36,24 @@ const App = () => {
     return null;
   };
 
+  const profileLoader = async ({ params }) => {
+    const endpoint = `profile/${params.username}`;
+    const response = await getRequest(endpoint);
+    return response;
+  };
+
   const routes = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/">
         <Route index element={<Home userAuth={userAuth} />} />
         <Route path="/login" loader={isUserAuth} exact element={<LogIn />} />
         <Route path="/signup" loader={isUserAuth} exact element={<SignUp />} />
+        <Route
+          path="/profile/:username"
+          exact
+          loader={profileLoader}
+          element={<Profile userAuth={userAuth} />}
+        />
       </Route>
     )
   );
