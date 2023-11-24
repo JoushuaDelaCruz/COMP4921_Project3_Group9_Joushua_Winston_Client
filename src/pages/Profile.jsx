28f1fa73ba64f27b2f2e19dafed4c8577a8bdbf5
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-23 18:43:17 -0800

    Created the placeholder for profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,40 +1,24 @@
-import { useState } from "react";
+import React, { useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
-import useRequest from "./customs/useRequest";
+import NavBar from "./components/NavBar";
 
-const profile = ({ userAuth }) => {
-  const [loader] = useLoaderData();
-  const [isAdded, setIsAdded] = useState(loader.is_added == 1);
-  const { postRequest } = useRequest();
-  const addFriendHandler = async () => {
-    const endpoint = `profile/addFriend/${loader.username}`;
-    const response = await postRequest(endpoint);
-    if (response.success) {
-      setIsAdded(true);
-    } else {
-      alert(response.message);
-    }
-  };
+const Profile = ({ user }) => {
+  const friends = useLoaderData();
+
+  useEffect(() => {
+    console.log(user);
+  }, []);
   return (
-    <div>
-      <header>Profile Page</header>
-      <h1>{loader.username}</h1>
-      {userAuth && (
-        <div>
-          {isAdded ? (
-            <button className=" px-4 py-2 bg-green-600 w-fit">Added</button>
-          ) : (
-            <button
-              onClick={addFriendHandler}
-              className="px-4 py-2 bg-blue-600 w-fit"
-            >
-              Add Friend
-            </button>
-          )}
-        </div>
-      )}
-    </div>
+    <main className="h-screen min-h-screen w-screen gap-5 relative flex flex-col sm:flex-row bg-mint-cream">
+      <nav className="w-full px-2 py-3 border-b shadow-md text-center bg-white sm:hidden">
+        <h1 className="text-3xl font-logo font-semibold text-celadon">
+          MakeItHappen
+        </h1>
+      </nav>
+      <NavBar currentPage={3} />
+      <section className="flex-1">hi!</section>
+    </main>
   );
 };
 
-export default profile;
+export default Profile;
