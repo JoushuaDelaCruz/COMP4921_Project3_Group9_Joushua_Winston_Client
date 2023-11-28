95a19ab5af65f57f34d781cf962f0bf0b9941df9
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-27 21:58:41 -0800

    Added the functionality of seeing all friends

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,15 +5,15 @@ import { Cloudinary } from "@cloudinary/url-gen";
 import { AdvancedImage } from "@cloudinary/react";
 import AddFriendCard from "./components/AddFriendCard";
 import useRequest from "./customs/useRequest";
+import FriendCard from "./components/FriendCard";
 
 const Profile = ({ user }) => {
-  const friends = useLoaderData();
+  const [friends, setFriends] = useState(useLoaderData());
   const [recommendedFriends, setRecommendedFriends] = useState([]);
   const [isAddUserTitleHidden, setIsAddUserTitleHidden] = useState(false);
   const [addUserAnimation, setAddUserAnimation] = useState("");
   const [searchBarAnimation, setSearchBarAnimation] = useState("opacity-0");
   const [searchFriendName, setSearchFriendName] = useState("");
-  const [AddedFriend, setAddedFriend] = useState([]);
   const { getRequest, postRequest } = useRequest();
   const cld = new Cloudinary({
     cloud: { cloudName: import.meta.env.VITE_CLOUD_NAME },
@@ -69,23 +69,33 @@ const Profile = ({ user }) => {
     showSearchBar();
   };
 
-  const addFriend = (id) => {
-    const response = postRequest("profile/addFriend", { friend_id: id });
+  const addFriendToFriendsAdded = (friend) => {
+    friend.date_added = new Date();
+    friend.accepted = 0;
+    const newFriends = [...friends, friend];
+    setFriends(newFriends);
+  };
+
+  const addFriend = (friend) => {
+    const response = postRequest("profile/addFriend", {
+      friend_id: friend.user_id,
+    });
     if (response) {
-      removeRecommendedFriend(id);
+      removeRecommendedFriend(friend.user_id);
+      addFriendToFriendsAdded(friend);
     }
   };
 
   return (
-    <main className="h-screen min-h-screen w-screen gap-5 relative flex flex-col sm:flex-row bg-mint-cream">
+    <main className="h-screen min-h-screen w-screen gap-5 relative flex flex-col sm:flex-row bg-mint-cream font-roboto">
       <nav className="w-full px-2 py-3 border-b shadow-md text-center bg-white sm:hidden">
         <h1 className="text-3xl font-logo font-semibold text-celadon">
           MakeItHappen
         </h1>
       </nav>
       <NavBar currentPage={3} />
-      <section className="flex-1 flex flex-col gap-5 mx-2">
-        <header className="flex flex-col gap-4 items-center sm:hidden">
+      <section className="flex-1 flex flex-col gap-5 mx-2 sm:py-6 sm:flex-row sm:justify-center lg:gap-20 sm:mt-8">
+        <header className="flex flex-col gap-4 items-center sm:order-2 sm:mt-10">
           <div className="flex items-center w-full gap-2">
             <AdvancedImage
               cldImg={cld.image(user.image)}
@@ -128,27 +138,40 @@ const Profile = ({ user }) => {
               />
             </div>
           </div>
-          <section className="flex flex-row gap-4 overflow-x-scroll py-2 w-full">
-            {recommendedFriends.map((friend) => {
-              return (
-                <AddFriendCard
-                  key={`recommended-${friend.user_id}`}
-                  user={friend}
-                  removeRecommendedFriend={removeRecommendedFriend}
-                  addFriend={addFriend}
-                />
-              );
-            })}
-            {recommendedFriends.length === 0 && (
+          <section className="flex flex-row gap-4 overflow-x-scroll py-2 w-full sm:flex-col sm:overflow-visible">
+            {recommendedFriends ? (
+              recommendedFriends.map((friend) => {
+                return (
+                  <AddFriendCard
+                    key={`recommended-${friend.user_id}`}
+                    user={friend}
+                    removeRecommendedFriend={removeRecommendedFriend}
+                    addFriend={addFriend}
+                  />
+                );
+              })
+            ) : (
               <h1 className="text-xs font-bold text-ash-grey w-full text-center">
                 No recommended friends
               </h1>
             )}
           </section>
         </header>
-        <section className="flex flex-col gap-2">
-          <div></div>
-          <div></div>
+        <section className="flex flex-col gap-2 sm:flex-1 sm:max-w-xl">
+          <h1 className="font-semibold px-4 text-mint-cream bg-feldgrau rounded-lg py-1">
+            Friends & Status
+          </h1>
+          <div className="flex flex-col py-3 gap-3">
+            {friends ? (
+              friends.map((friend, index) => {
+                return <FriendCard key={`friend-${index}`} friend={friend} />;
+              })
+            ) : (
+              <h1 className="text-xs font-bold w-full text-center">
+                Add more friends!
+              </h1>
+            )}
+          </div>
         </section>
       </section>
     </main>
