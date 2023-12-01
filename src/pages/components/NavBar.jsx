f4de1cc6e6b153b69d7574cb59c84da400b467ec
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-30 19:59:05 -0800

    Added a h-full size for larger screen

--- src/pages/components/NavBar.jsx
+++ src/pages/components/NavBar.jsx
@@ -24,13 +24,13 @@ const NavBar = ({ currentPage }) => {
   ];
 
   return (
-    <div className="fixed bottom-0 w-full flex bg-white px-5 py-2 border-t border-ash-grey sm:relative sm:bottom-auto sm:w-60 sm:flex-col sm:justify-start sm:gap-3 sm:border-r sm:border-t-0">
-      <div className="hidden sm:block py-4">
+    <div className="fixed bottom-0 w-full md:h-full flex bg-white px-5 py-2 border-t border-ash-grey md:relative md:bottom-auto md:w-60 md:flex-col md:justify-start md:gap-3 md:border-r md:border-t-0">
+      <div className="hidden md:block py-4">
         <h1 className="text-3xl font-logo font-semibold text-celadon">
           MakeItHappen
         </h1>
       </div>
-      <div className="flex justify-between w-full sm:justify-start sm:flex-col gap-5">
+      <div className="flex justify-between w-full md:justify-start md:flex-col gap-5">
         {pages.map((page) => (
           <NavbarButtons
             key={page.id}
