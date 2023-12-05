26b2889bc5d8ecf17c35d52c71b8fa13f5dbebf5
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-12-04 23:55:11 -0800

    Added link props

--- src/pages/components/NavbarButtons.jsx
+++ src/pages/components/NavbarButtons.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const NavbarButtons = ({ icon, isActive, order, description }) => {
+const NavbarButtons = ({ icon, isActive, order, description, link }) => {
   const [isHovered, setIsHovered] = useState(false);
   return (
     <Link
-      to={`/${description === "calendar" ? "" : description}`}
+      to={link}
       disabled={isActive}
       className={`p-2 text-feldgrau rounded-full gap-5 md:order-${order} md:flex md:items-center md:rounded-md`}
       onMouseEnter={() => setIsHovered(true)}
