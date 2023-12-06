1fcb1fec25e71b84de029a5cfe9c1c1fa2a8a797
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-12-05 21:47:59 -0800

    Make description or location to not show if not provided

--- src/pages/components/EventBinCard.jsx
+++ src/pages/components/EventBinCard.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import useEventCountDown from "../customs/useEventCountDown";
 import useEventDuration from "../customs/useDurationFormat";
 import useRequest from "../customs/useRequest";
+import useDateFormat from "../customs/useDateFormat";
 
 const EventBinCard = ({ index, event, removeEvent }) => {
   const { postRequest, deleteRequest } = useRequest();
@@ -14,22 +15,26 @@ const EventBinCard = ({ index, event, removeEvent }) => {
     }, 250 * index);
     return () => clearTimeout(timeout);
   }, [setIsLoading]);
-  
+
   const handleRestore = async () => {
-    const result = await postRequest("recycle/restoreEvent", {uuid: event.uuid});
-    if (!result) alert("Couldn't restore event. Please try again later.")
+    const result = await postRequest("recycle/restoreEvent", {
+      uuid: event.uuid,
+    });
+    if (!result) alert("Couldn't restore event. Please try again later.");
     handleRemoveEvent();
-  }
+  };
 
   const handleDelete = async () => {
-    const result = await deleteRequest("recycle/deleteEvent", {uuid: event.uuid});
-    if (!result) alert("Couldn't delete event. Please try again later.")
+    const result = await deleteRequest("recycle/deleteEvent", {
+      uuid: event.uuid,
+    });
+    if (!result) alert("Couldn't delete event. Please try again later.");
     handleRemoveEvent();
-  }
+  };
 
   const handleRemoveEvent = () => {
     removeEvent(event.uuid);
-  }
+  };
 
   return (
     <article
@@ -45,23 +50,44 @@ const EventBinCard = ({ index, event, removeEvent }) => {
         <h1 className="text-base font-bold text-gray-600">
           Event: {event.title}
         </h1>
-        <h2 className="text-xs italic"> Deleted {useEventDuration(event.recycle_datetime, new Date())} ago </h2>
+        <h2 className="text-xs italic">
+          {" "}
+          Deleted {useEventDuration(
+            event.recycle_datetime,
+            new Date()
+          )} ago{" "}
+        </h2>
       </header>
       <div className="flex flex-col gap-1 text-sm">
-        <h2> Location: {event.location} </h2>
-        <h2> Created: {useEventDuration(event.created_datetime, new Date())} </h2>
+        {event.location && <h2> Location: {event.location} </h2>}
+        <h2> Created: {useDateFormat(event.created_datetime)} </h2>
         <h2> {useEventCountDown(event.start_datetime)} </h2>
-        <h2> Duration: {useEventDuration(event.start_datetime, event.end_datetime)} </h2>
-        <p className="font-light">
-          <span className="font-normal">Description:</span> {event.description}
-        </p>
+        <h2>
+          {" "}
+          Duration: {useEventDuration(
+            event.start_datetime,
+            event.end_datetime
+          )}{" "}
+        </h2>
+        {event.description && (
+          <p className="font-light">
+            <span className="font-normal">Description:</span>{" "}
+            {event.description}
+          </p>
+        )}
       </div>
       <footer className="flex gap-2 mt-2">
-        <button onClick={handleRestore} className="px-6 py-2 w-full bg-celadon/60 cursor-pointer rounded-md hover:bg-celadon/80 active:bg-celadon/90 focus:outline-none focus:ring focus:ring-celadon transition-all duration-300">
+        <button
+          onClick={handleRestore}
+          className="px-6 py-2 w-full bg-celadon/60 cursor-pointer rounded-md hover:bg-celadon/80 active:bg-celadon/90 focus:outline-none focus:ring focus:ring-celadon transition-all duration-300"
+        >
           {" "}
           Restore{" "}
         </button>
-        <button onClick={handleDelete} className="px-6 py-2 w-full rounded-md cursor-pointer bg-ash-grey/60 hover:bg-ash-grey/80 active:bg-ash-grey/90 focus:outline-none focus:ring focus:ring-ash-grey transition-all duration-300">
+        <button
+          onClick={handleDelete}
+          className="px-6 py-2 w-full rounded-md cursor-pointer bg-ash-grey/60 hover:bg-ash-grey/80 active:bg-ash-grey/90 focus:outline-none focus:ring focus:ring-ash-grey transition-all duration-300"
+        >
           {" "}
           Delete{" "}
         </button>
